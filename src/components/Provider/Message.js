efe5bd3bfb650590ad41484d37062cdc298870cf
noksenberg <niv.oksenberg@getjusto.com>
2021-05-10 15:45:25 -0400

    style: override notification z-index

--- src/components/Provider/Message.js
+++ src/components/Provider/Message.js
@@ -78,6 +78,11 @@ export default class WithMessage extends React.Component {
 
   getStyle() {
     return {
+      Containers: {
+        DefaultStyle: {
+          zIndex: 10001,
+        },
+      },
       NotificationItem: {
         DefaultStyle: {
           margin: '10px 5px 2px 1px',
